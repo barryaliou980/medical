875b9df42a2975462f0f43d09314d11cae36d15a
Aliou Barry <barryaliou980@gmail.com>
2021-06-19 11:49:36 +0000

    test 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{asset("assets/css/styles.css")}}" rel="stylesheet" />
+        <link href="{{secure_asset("assets/css/styles.css")}}" rel="stylesheet" />
     </head>
     <body id="page-top">
         <!-- Navigation-->
@@ -27,6 +27,6 @@
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="{{asset('assets/js/scripts.js')}}"></script>
+        <script src="{{secure_asset('assets/js/scripts.js')}}"></script>
     </body>
 </html>
